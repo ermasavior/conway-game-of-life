224dea9b86b430fe20cccbc70454c511621ab467
Erma <13516072@std.stei.itb.ac.id>
2021-09-19 21:39:47 +0700

    feat: add App entry point

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        String[] worldMap = {
+                "...O...",
+                "...O...",
+                "...O..."
+        };
+
+        Game game = new Game();
+        game.loadUniverse(worldMap);
+        game.printUniverse();
+    }
+}
